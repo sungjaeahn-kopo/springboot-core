3bf7061895671e4b1f33d52180866f779c1a721a
sungjaeahn-kopo <dtw421@naver.com>
2025-01-02 17:13:29 +0900

    Conver to Spring

--- src/main/java/core/spring/AppConfig.java
+++ src/main/java/core/spring/AppConfig.java
@@ -9,25 +9,33 @@ import core.spring.member.MemberServiceImpl;
 import core.spring.member.MemoryMemberRepository;
 import core.spring.order.OrderService;
 import core.spring.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * DI Container(IoC Container)
  *
  * 역할과 구현의 분리
  * 어플리케이션의 전체 구성을 파악하기 좋음.
+ *
+ * @Bean 어노테이션 추가시, spring container에 등록됨.
  */
+@Configuration
 public class AppConfig {
 
     // 리팩토링 단축키: ctrl + alt + m
     // 생성자 주입 방식
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemberRepository memberRepository() {
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
@@ -36,6 +44,7 @@ public class AppConfig {
      * 정책이 변경되어도, 사용영역에서 변경하지 않고, 구성영역에서만(Appconfig) 변경하면 된다.
      * @return
      */
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/core/spring/MemberApp.java
+++ src/main/java/core/spring/MemberApp.java
@@ -4,13 +4,20 @@ import core.spring.member.Grade;
 import core.spring.member.Member;
 import core.spring.member.MemberService;
 import core.spring.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+        // ApplicationContext가 spring container에 넣어주는 역할
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
